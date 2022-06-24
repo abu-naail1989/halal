9ccabc948143ec0ec0fef8b4a1a42e8c652f954d
James Alhassan <jamesalhassan052@gmail.com>
2022-06-24 07:17:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+<<<<<<< HEAD
+=======
+//setup date
+const date = document.getElementById('date');
+if (date) {
+  date.innerHTML = new Date().getFullYear();
+}
+>>>>>>> 250b2ab54e22aebc522c0a22fe12b180708259a9
 // setup nav
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
+const navBtn = document.getElementById('nav-btn');
+const navbar = document.getElementById('navbar');
+const navClose = document.getElementById('nav-close');
 // show nav
-navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
-});
+if (navBtn) {
+  navBtn.addEventListener('click', () => {
+    navbar.classList.add('showNav');
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navbar.classList.remove('showNav');
+  });
+}
 // close nav
+<<<<<<< HEAD
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
@@ -14,3 +31,5 @@ navClose.addEventListener("click", () => {
 const date = (document.getElementById(
   "date"
 ).innerHTML = new Date().getFullYear());
+=======
+>>>>>>> 250b2ab54e22aebc522c0a22fe12b180708259a9
